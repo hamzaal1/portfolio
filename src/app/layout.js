eda1a110ccf4662989b4ea39c0b0f05ad89045fc
D.hamza <hamzademnati21@gmail.com>
2023-07-03 00:38:23 +0200

    add new preview image

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ import Head from 'next/head'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'TechStylitix : Front-End | Back-End | Make Web Application | Bug Solver',
+  title: 'TechStylitix |Senior Next Js & React Js | Front-End | Back-End | Make Web Application | Bug Solver',
   description: 'Hi , I’m Hamza Demnani 😃 .i have over 3years Of Designing end- to - end experience with 🖤 for financial products on web platforms.Working closely with managers, marketing specialists and developers. 🌟',
 }
 
@@ -17,9 +17,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        {/* <meta property="og:image" content="/TechStylitix.png" />
+        <meta property="og:image" content="/TechStylitix.png" />
         <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" /> */}
+        <meta property="og:image:height" content="630" />
       </Head>
       <body className={`${inter.className} bg-background md:overflow-y-auto`}>
         <LoadAnimation />
