6f398aec4255ed9663c6adefa4cad548c1c8a4f5
D.hamza <hamzademnati21@gmail.com>
2023-07-02 20:10:20 +0200

    new update 1.1

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta property="og:image" content="/TechStylitix.png" />
+        {/* <meta property="og:image" content="/TechStylitix.png" />
         <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="og:image:height" content="630" /> */}
       </Head>
       <body className={`${inter.className} bg-background md:overflow-y-auto`}>
         <LoadAnimation />
