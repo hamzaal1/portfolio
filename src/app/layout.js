c3702c656cdfce692fdae571cac6e98ffa180b76
D.hamza <hamzademnati21@gmail.com>
2023-07-10 20:51:41 +0200

    fix google analytics not taged

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import LoadAnimation from '@/components/load'
 import CopyRight from '@/components/copyright'
-import Analytics from '@/components/analytics'
+import Script from 'next/script'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,13 +15,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
+        `,
+        }}
+      />
       <meta property="og:image" content="/TechStylitix.svg" />
       <meta name="keywords" content={metadata.keywords} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <body className={`${inter.className} bg-background relative overflow-x-hidden md:overflow-y-auto`}>
         <LoadAnimation />
-        <Analytics />
         <Navbar />
         {children}
         <CopyRight />
